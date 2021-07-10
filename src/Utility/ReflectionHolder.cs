53f29f52c60abe63fb787e076e2d6526cfea3efd
Andrea Angelini <andrea-angelini@outlook.it>
2021-07-10 21:11:57 +0200

    + avoid reflections for properties

--- src/Utility/ReflectionHolder.cs
+++ src/Utility/ReflectionHolder.cs
@@ -38,47 +38,12 @@ namespace ClassicUO.Utility
 {
     internal static class ReflectionHolder
     {
-        public static Dictionary<string, string> GetGameObjectProperties<T>(T obj) where T : GameObject
-        {
-            PropertyInfo[] props = obj?.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        //public static Dictionary<string, string> GetGameObjectProperties<T>(T obj) where T : GameObject
+        //{
+            
 
-            FieldInfo[] fields = obj?.GetType().GetFields(BindingFlags.Public | BindingFlags.Instance);
-
-            Dictionary<string, string> dict = new Dictionary<string, string>();
-
-            if (props != null)
-            {
-                foreach (PropertyInfo prop in props)
-                {
-                    if (prop.PropertyType.IsByRef)
-                    {
-                    }
-                    else
-                    {
-                        object value = prop.GetValue(obj, null);
-
-                        dict[prop.Name] = value == null ? "null" : value.ToString();
-                    }
-                }
-            }
-
-            if (fields != null)
-            {
-                foreach (FieldInfo prop in fields)
-                {
-                    if (prop.FieldType.IsByRef)
-                    {
-                    }
-                    else
-                    {
-                        object value = prop.GetValue(obj);
-
-                        dict[prop.Name] = value == null ? "null" : value.ToString();
-                    }
-                }
-            }
-
-            return dict;
-        }
+          
+        //    return dict;
+        //}
     }
 }
